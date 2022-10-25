39764b89db74253e597d7ec64a7ae18c65e9fb55
Zach <54410451+zach2y@users.noreply.github.com>
2022-10-25 14:54:59 +0800

    Update COVID-Data-Exploration.sql

--- COVID-Data-Exploration/COVID-Data-Exploration.sql
+++ COVID-Data-Exploration/COVID-Data-Exploration.sql
@@ -1,9 +1,9 @@
 /*
-Covid 19 Data Exploration 
+Data Exploration of COVID-19 in Malaysia
 
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
-Link to Datasets: https://github.com/MoH-Malaysia/covid19-public (Up to 21 October 2022)
+Link to Datasets: https://github.com/MoH-Malaysia/covid19-public (Downloaded on 22 October 2022)
 */
 
 -- Check tables
